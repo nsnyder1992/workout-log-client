5b71c42482fec0dd40ae49efdea620d54ffe24c3
Nick Snyder <nsnyder1992@gmail.com>
2021-02-02 11:38:03 -0500

    added client

---
+++ client/workoutlog/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+import Sitebar from "./home/Navbar";
+import Auth from "./auth/Auth";
+
+function App() {
+  document.title = "Workout Log Client";
+
+  const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+
+  return (
+    <div>
+      <Sitebar />
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
